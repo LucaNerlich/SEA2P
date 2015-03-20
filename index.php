63b180e558ae0c4d01dd5a73fa0321ff18b516e2
Fabian <Fabian@141.22.94.201>
2015-03-20 18:36:12 +0100

    Adding DB

--- index.php
+++ index.php
@@ -8,7 +8,9 @@
 	echo "<h1>Beispiel PHP-Datei</h1>";
 	
 	echo "<h2>Datenbankverbindung</h2>";
-	
+
+	$DB = mysqli_connect ("eu-cdbr-azure-north-c.cloudapp.net", "b9e898e7fd354b", "603aec01") or die ("Keine Verbindung moeglich!");
+	mysqli_select_db ($DB, HAWBIKEDB1) or die ("Die Datenbank existiert nicht.");
 ?>
 	</body>
 </html>
