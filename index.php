39d66233dcea457db8b80c56a91fc8e55d406c84
Fabian <Fabian@141.22.94.201>
2015-03-20 18:41:59 +0100

    Test

--- index.php
+++ index.php
@@ -11,9 +11,13 @@
 
 	$DB = mysqli_connect ("eu-cdbr-azure-north-c.cloudapp.net", "b9e898e7fd354b", "603aec01") or die ("Keine Verbindung moeglich!");
 	mysqli_select_db ($DB, HAWBIKEDB1) or die ("Die Datenbank existiert nicht.");
-	mysqli_query($DB, "create table if not exists user (user_id int not null AUTO_INCREMENT, name varchar(50), email varchar(50), type int not null default 2, primary key (user_id))");
-	mysqli_query($DB,"INSERT INTO user (name, email) VALUES ('Fabian','fabiansim@gmx.de'),('Testperson','test@test.de');");
-	
+	//mysqli_query($DB, "create table if not exists user (user_id int not null AUTO_INCREMENT, name varchar(50), email varchar(50), type int not null default 2, primary key (user_id))");
+	//mysqli_query($DB,"INSERT INTO user (name, email) VALUES ('Fabian','fabiansim@gmx.de'),('Testperson','test@test.de');");
+	$result = mysqli_query("SELECT * FROM user");
+	while ($ar = mysqli_fetch_assoc($result))
+	{
+		echo "<p>#" . $ar["user_id"] . ": " . $ar["name"] . " (" . $ar["email"] . ")";
+	}
 ?>
 	</body>
 </html>
