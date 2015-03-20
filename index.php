17d5ece686269e1d8066b5cba89f3fb6a142fa4d
Fabian <Fabian@141.22.94.201>
2015-03-20 18:35:10 +0100

    Test PHP

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+	<head>
+		<title>PHP-Beispiel</title>
+		
+	</head>
+	<body>
+<?php
+	echo "<h1>Beispiel PHP-Datei</h1>";
+	
+	echo "<h2>Datenbankverbindung</h2>";
+	
+?>
+	</body>
+</html>
