57dd1aded3718ec82c149426fc3af4ce81176df8
Fabian <Fabian@Laptop>
2015-03-26 15:40:30 +0100

    Header + Footer

--- index.php
+++ index.php
@@ -1,29 +1,50 @@
-<html>
-	<head>
-		<title>PHP-Beispiel</title>
-	</head>
-	<body>
-<?php
-	echo "<h1>Beispiel PHP-Datei</h1>";
-	
-	echo "<h2>Datenbankverbindung</h2>";
+<?php 
+session_start();
+if (!isset($_SESSION["state"]))
+{
+	$_SESSION["state"] = 0; // 0 = Not logged in, 1 = Logged In as Customer, 2 = Logged In as Admin
+}
 
-	$DB = mysqli_connect ("eu-cdbr-azure-north-c.cloudapp.net", "b9e898e7fd354b", "603aec01") or die ("Keine Verbindung moeglich!");
-	mysqli_select_db ($DB, HAWBIKEDB1) or die ("Die Datenbank existiert nicht.");
-	if (!mysqli_query($DB, "create table if not exists user (user_id int not null AUTO_INCREMENT, name varchar(50), email varchar(50), type int not null default 2, primary key (user_id))"))
-	{
-		echo "Es trat ein Fehler auf beim Anlegen: " . mysqli_error($DB);
-	}
-	if (!mysqli_query($DB,"INSERT INTO user (name, email) VALUES ('Fabian','fabiansim@gmx.de'),('Testperson','test@test.de');"))
-	{
-		echo "Es trat ein Fehler auf beim Insert: " . mysqli_error($DB);
-	}
-	echo "<h2>Ausgabe</h2>";
-	$result = mysqli_query($DB,"SELECT * FROM user");
-	while ($ar = mysqli_fetch_assoc($result))
-	{
-		echo "<p>#" . $ar["user_id"] . ": " . $ar["name"] . " (" . $ar["email"] . ")";
-	}
+include "spielwiese/config.php";
+include "spielwiese/header.php";
+
+if (isset($_GET["page"]) && file_exists($_GET["page"]))
+{
+	include $_GET["page"];
+}
+else
+{
+	echo '<div class="jumbotron">
+  <h1>HAW Bike Tracking</h1>
+  <p>Software Entwicklung 2</p>
+  <p><a class="btn btn-primary btn-lg" href="src/learnMore.html" role="button">Unser Konzept</a></p>
+</div>';
+}
+
+include "spielwiese/footer.php";
+
+
 ?>
-	</body>
-</html>
+
+<?php
+// 	echo "<h1>Beispiel PHP-Datei</h1>";
+	
+// 	echo "<h2>Datenbankverbindung</h2>";
+
+// 	$DB = mysqli_connect ("eu-cdbr-azure-north-c.cloudapp.net", "b9e898e7fd354b", "603aec01") or die ("Keine Verbindung moeglich!");
+// 	mysqli_select_db ($DB, HAWBIKEDB1) or die ("Die Datenbank existiert nicht.");
+// 	if (!mysqli_query($DB, "create table if not exists user (user_id int not null AUTO_INCREMENT, name varchar(50), email varchar(50), type int not null default 2, primary key (user_id))"))
+// 	{
+// 		echo "Es trat ein Fehler auf beim Anlegen: " . mysqli_error($DB);
+// 	}
+// 	if (!mysqli_query($DB,"INSERT INTO user (name, email) VALUES ('Fabian','fabiansim@gmx.de'),('Testperson','test@test.de');"))
+// 	{
+// 		echo "Es trat ein Fehler auf beim Insert: " . mysqli_error($DB);
+// 	}
+// 	echo "<h2>Ausgabe</h2>";
+// 	$result = mysqli_query($DB,"SELECT * FROM user");
+// 	while ($ar = mysqli_fetch_assoc($result))
+// 	{
+// 		echo "<p>#" . $ar["user_id"] . ": " . $ar["name"] . " (" . $ar["email"] . ")";
+// 	}
+?>
