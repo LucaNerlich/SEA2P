4bbbec02193aa97bf1052c41c881b719f2e7da92
Luca Nerlich <luca.nerlich@googlemail.com>
2015-04-17 23:40:27 +0200

    slider finally works

--- src/presentation2.php
+++ src/presentation2.php
@@ -15,54 +15,91 @@ include '../config/header.php';
         <link rel="icon" href="/favicon.ico">
 
         <title>Presentation 1</title>
-        <link rel="stylesheet" media="screen" href="../css/sequencejs-theme.modern-slide-in.css"/>
-        <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Ruluko|Sirin+Stencil">
+        <link rel="stylesheet" type="text/css" media="screen" href="../css/sequencejs-theme.sliding-horizontal-parallax.css" />
+        <link href="http://fonts.googleapis.com/css?family=Play:400,700" rel="stylesheet" type="text/css">
 
-        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-
-        <script src="../js/responsiveslides.min.js"></script>
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
 
         <script>
-            if (typeof jQuery == 'undefined') {
+            if (typeof jQuery == 'undefined'){
                 document.write(unescape('%3Cscript src="scripts/jquery-min.js" %3E%3C/script%3E'));
             }
         </script>
-
         <script src="../js/jquery.sequence-min.js"></script>
-        <script src="../js/sequencejs-options.modern-slide-in.js"></script>
+        <script src="../js/sequencejs-options.sliding-horizontal-parallax.js"></script>
 
     </head>
     <body>
-    <div class="sequence-theme">
-        <div id="sequence">
 
-            <img class="sequence-prev" src="../assets/images/ppp1/bt-prev.png" alt="Previous Frame"/>
-            <img class="sequence-next" src="../assets/images/ppp1/bt-next.png" alt="Next Frame"/>
+    <div id="sequence">
+        <img class="sequence-prev" src="../assets/images/ppp1/bt-prev.png" alt="Previous" />
+        <img class="sequence-next" src="../assets/images/ppp1/bt-next.png" alt="Next" />
 
-            <ul class="sequence-canvas">
-                <li class="animate-in">
-                    <h2 class="title">Built using Sequence.js</h2>
+        <ul class="sequence-canvas">
+            <li class="animate-in">
+                <div class="info">
+                </div>
+                <img class="sky" src="../assets/images/ppp1/Folie1.PNG" alt="Blue Sky" />
+            </li>
+            <li>
+                <div class="info">
+                </div>
+                <img class="sky" src="../assets/images/ppp1/Folie2.PNG" alt="Blue Sky" />
+            </li>
+            <li>
+                <div class="info">
+                </div>
+                <img class="sky" src="../assets/images/ppp1/Folie3.PNG" alt="Blue Sky" />
+            </li>
+            <li>
+                <div class="info">
+                </div>
+                <img class="sky" src="../assets/images/ppp1/Folie4.PNG" alt="Blue Sky" />
+            </li>
+            <li>
+                <div class="info">
+                </div>
+                <img class="sky" src="../assets/images/ppp1/Folie5.PNG" alt="Blue Sky" />
+            </li>
+            <li>
+                <div class="info">
+                </div>
+                <img class="sky" src="../assets/images/ppp1/Folie6.PNG" alt="Blue Sky" />
+            </li>
+            <li>
+                <div class="info">
+                </div>
+                <img class="sky" src="../assets/images/ppp1/Folie7.PNG" alt="Blue Sky" />
+            </li>
+            <li>
+                <div class="info">
+                </div>
+                <img class="sky" src="../assets/images/ppp1/Folie8.PNG" alt="Blue Sky" />
+            </li>
+            <li>
+                <div class="info">
+                </div>
+                <img class="sky" src="../assets/images/ppp1/Folie9.PNG" alt="Blue Sky" />
+            </li>
+            <li>
+                <div class="info">
+                </div>
+                <img class="sky" src="../assets/images/ppp1/Folie10.PNG" alt="Blue Sky" />
+            </li>
+            <li>
+                <div class="info">
+                </div>
+                <img class="sky" src="../assets/images/ppp1/Folie11.PNG" alt="Blue Sky" />
+            </li>
+            <li>
+                <div class="info">
+                </div>
+                <img class="sky" src="../assets/images/ppp1/Folie12.PNG" alt="Blue Sky" />
+            </li>
 
-                    <h3 class="subtitle">The Responsive Slider with Advanced CSS3 Transitions</h3>
-                    <img class="model" src="../assets/images/ppp1/Folie1.PNG" alt="Model 1"/>
-                </li>
-                <li>
-                    <h2 class="title">Creative Control</h2>
-
-                    <h3 class="subtitle">Create unique sliders using CSS3 transitions</h3>
-                    <img class="model" src="../assets/images/ppp1/Folie2.PNG" alt="Model 2"/>
-                </li>
-                <li>
-                    <h2 class="title">Cutting Edge</h2>
-
-                    <h3 class="subtitle">Supports modern browsers, old browsers (IE7+), touch devices and responsive
-                        designs</h3>
-                    <img class="model" src="../assets/images/ppp1/Folie3.PNG" alt="Model 3"/>
-                </li>
-            </ul>
-
-        </div>
+        </ul>
     </div>
+
     </body>
     </html>
 <?php
