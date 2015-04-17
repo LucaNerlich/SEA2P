469a5a554d3bfca8e3c8f578f038ebb869b30e05
Clemens Raßbach <clemens.rassbach@haw-hamburg.de>
2015-04-17 22:41:48 +0200

    test

--- form.php
+++ form.php
@@ -2,30 +2,42 @@
 include 'config/config.php';
 include 'config/header.php';
 
-echo '<div id="damagereport">';
-echo '<form action="form.php">';
-echo '            <select name="type" size="1">';
+if ($_SESSION["state"] == 0) {
+
+    echo '<div id="damagereport">';
+    echo '<form action="form.php" method="post">';
+    echo '            <select name="type" size="1">';
 //echo '                <option>---</option>';
-echo '                <option>Schadenmeldung</option>';
+    echo '                <option>Schadenmeldung</option>';
 //echo '               <option>Message</option>';
-echo '            </select>';
+    echo '            </select>';
 
-echo '<br>';
+    echo '<br>';
 
-echo '            <select name="product" size="1">';
-echo '                <option>Produkt ausw&auml;hlen</option>';
-echo '                <option>$product1</option>';
-echo '               <option>$product1</option>';
-echo '            </select>';
+/* BIKE */
 
-echo '<br>';
+    $bikes = query("SELECT serial_number FROM bike WHERE bike.client_id = " . $_SESSION["user"]["client_id"]);
 
-echo '            <label for="serial">S/N:</label> ';
-echo '            <input type="text" id="serial" size="10" maxlength="10" pattern="[0-9]{10}">/>';
+    echo '            <label for="bike">Bike:</label> ';
+    echo '            <select name="bike" size="1">';
+    echo '                <option>Bike ausw&auml;hlen</option>';
 
-echo '            <input type="submit" value="send">';
-echo '        </form>';
-echo '</div>';
+    foreach ($bikes as $bike) {
+        echo '<option>' . $bike . '</option>';
+    }
+
+    echo '            </select>';
+
+    echo '<br>';
+
+    echo '            <label for="serial">S/N:</label> ';
+    echo '            <input type="text" id="serial" size="10" maxlength="10" pattern="[0-9]{10}">';
+
+    echo '            <input type="submit" value="send">';
+    echo '        </form>';
+    echo '</div>';
+
+}
 
 include 'config/footer.php';
 ?>
