35f349d1cccb94a006b9f0b41cf677f537dda3e3
Fabian <Fabian@Laptop>
2015-04-17 21:25:08 +0200

    Gelsöcht

--- test.php
+++ test.php
@@ -2,6 +2,25 @@
 <head>
 </head>
 <body>
-Hallo 
+
+<?php
+include ("spielwiese/config.php");
+
+// $var = mysqli_query($DB, "SELECT * FROM client");
+// //var_dump($var);
+// while ($zeile = mysqli_fetch_assoc($var))
+// {
+// 	echo $zeile["surname"] . "<br>";
+// }
+$new_id = query("INSERT INTO client (surname, name, email,password) values ('Person','Nachname','test@mail.de','asd')");
+echo $new_id . "<br>";
+$clients = query("SELECT * FROM client");
+
+foreach ($clients as $client)
+{
+	echo $client["surname"] . "<br>";
+}
+
+?>
 </body>
 </html>
