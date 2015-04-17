baa299e1a211a5588fe325648137d8a62b8ff40d
Clemens Raßbach <clemens.rassbach@haw-hamburg.de>
2015-04-17 21:50:35 +0200

    test

--- form.php
+++ form.php
@@ -7,9 +7,9 @@ echo "            <select name="type" size="1">";
 echo "                <option>---</option>";
 echo "                <option>Schadenmeldung</option>";
 echo "                <option>Message</option>";
-echo "            </select>";"
+echo "            </select>";
 
-echo "            <input type="submit" value="send">";"
+echo "            <input type="submit" value="send">";
 echo "        </form>";
 
 include 'config/footer.php';
