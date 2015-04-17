8cece40139612abd53d967752594b9e5027a7606
Fabian <Fabian@Laptop>
2015-04-17 21:31:07 +0200

    Verschoben

---
+++ src/trash/Main.java
@@ -0,0 +1,12 @@
+/*
+SEA 2 Projekt
+Fabian S, Clemens R, Stefan Z, Daniel S & Luca N
+ */
+
+public class Main{
+    public static void main(String[] args) {
+        //test
+    }
+}
+        // git config --global user.email "you@example.com"
+        // git config --global user.name "Your Name"
