cd1317a93dd40c20d6cd1323a2b9735078d31dc6
LucaNerlich <luca.nerlich@googlemail.com>
2015-04-18 16:19:00 +0200

    added margin to register.php

--- src/register.php
+++ src/register.php
@@ -9,43 +9,43 @@ if ($_SESSION["state"] == 1 && (($_SESSION["user"]["email"] == "luca.nerlich@haw
     echo '<div style="width: 90%">';
     echo "<form class='form-signin' action='register.php' method='post'>";
 
-    echo '<label for="inputEmail" class="sr-only">Email address</label>';
-    echo '<input type="email" name="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>';
+    echo '<label for="inputEmail" class="sr-only">E-Mail address</label>';
+    echo '<input type="email" name="email" id="inputEmail" style="margin: 2px" class="form-control" placeholder="E-Mail address" required autofocus>';
 
     echo '<label for="inputPassword" class="sr-only">Password</label>';
-    echo '<input type="password" id="inputPassword" name="password" class="form-control" placeholder="Password" required>';
+    echo '<input type="password" id="inputPassword" style="margin: 2px" name="password" class="form-control" placeholder="Password" required>';
 
     echo '<label for="surname" class="sr-only">Surname</label>';
-    echo '<input type="text" name="surname" class="form-control" placeholder="Surname" aria-describedby="basic-addon1" required>';
+    echo '<input type="text" id="surname" name="surname" style="margin: 2px" class="form-control" placeholder="Surname" aria-describedby="basic-addon1" required>';
 
     echo '<label for="firstname" class="sr-only">Firstname</label>';
-    echo '<input type="text" name="firstname" class="form-control" placeholder="Firstname" aria-describedby="basic-addon1" required>';
+    echo '<input type="text" id="firstname" name="firstname" style="margin: 2px" class="form-control" placeholder="Firstname" aria-describedby="basic-addon1" required>';
 
     echo '<label for="streetname" class="sr-only">Streetname</label>';
-    echo '<input type="text" name="streetname" class="form-control" placeholder="Streetname" aria-describedby="basic-addon1" required>';
+    echo '<input type="text" id="streetname" name="streetname" style="margin: 2px" class="form-control" placeholder="Streetname" aria-describedby="basic-addon1" required>';
 
     echo '<div class="input-group">';
     echo '<label for="streetnumber" class="sr-only">Streetname</label>';
     echo '<span class="input-group-addon">No.</span>';
-    echo ' <input type="number" name="streetnumber" class="form-control" placeholder="Streetnumber" aria-label="Streetnumber" min="1" max="999" required>';
+    echo ' <input type="number" id="streetnumber" name="streetnumber" style="margin: 2px" class="form-control" placeholder="Streetnumber" aria-label="Streetnumber" min="1" max="999" required>';
     echo '</div>';
 
     echo '<div class="input-group">';
     echo '<label for="zipcode" class="sr-only">Zipcode</label>';
     echo '<span class="input-group-addon">Zip.</span>';
-    echo ' <input type="number" name="zipcode" class="form-control" placeholder="Zipcode" aria-label="Zipcode" min="0" max="99999" required>';
+    echo ' <input type="number" id="zipcode" name="zipcode" style="margin: 2px" class="form-control" placeholder="Zipcode" aria-label="Zipcode" min="0" max="99999" required>';
     echo '</div>';
 
     echo '<label for="city" class="sr-only">City</label>';
-    echo '<input type="text" name="city" class="form-control" placeholder="City" aria-describedby="basic-addon1" required>';
+    echo '<input type="text" id="city" name="city" style="margin: 2px" class="form-control" placeholder="City" aria-describedby="basic-addon1" required>';
 
     echo '<div class="input-group">';
     echo '<label for="telephoneNumber" class="sr-only">Telephone No.</label>';
     echo '<span class="input-group-addon">Tel. No.</span>';
-    echo ' <input type="text" name="telephoneNumber" class="form-control" placeholder="Telephone No." aria-label="Telephone No.">';
+    echo ' <input type="text" id="telephoneNumber" name="telephoneNumber" style="margin: 2px" class="form-control" placeholder="Telephone No." aria-label="Telephone No.">';
     echo '</div>';
 
-    echo '<button class="btn btn-lg btn-primary btn-block" type="submit">Register User</button>';
+    echo '<button class="btn btn-lg btn-primary btn-block" type="submit" style="margin: 2px">Register User</button>';
     echo '</div>';
     echo "</form>";
 } else {
