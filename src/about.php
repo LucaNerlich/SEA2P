e92428c475845dca223bfd48efd7013183f68a25
Fabian <Fabian@Laptop>
2015-04-18 13:36:10 +0200

    HTML Tags rausgenommen

--- src/about.php
+++ src/about.php
@@ -2,33 +2,6 @@
 include '../config/config.php';
 include '../config/header.php';
 ?>
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-        <meta name="description" content="">
-        <meta name="author" content="">
-        <link rel="icon" href="/favicon.ico">
-
-        <title>Learn More</title>
-
-        <link href="/css/bootstrap.css" rel="stylesheet">
-        <link href="/css/bootstrap-custom.css" rel="stylesheet">
-        <link href="/css/slider.css" rel="stylesheet">
-
-
-        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-        <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-        <![endif]-->
-    </head>
-    <body>
-    <br>
 
     <div class="panel panel-default" align="center">
         <div class="panel-heading">
@@ -162,15 +135,6 @@ include '../config/header.php';
 
 
     </div>
-
-
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="/js/bootstrap.min.js"></script>
-    </body>
-    </html>
-
 <?php
 include '../config/footer.php';
 ?>
