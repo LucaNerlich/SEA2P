c9e975855dabae7b48fdf6cda538c848aabf33f9
LucaNerlich <luca.nerlich@googlemail.com>
2015-04-20 22:29:26 +0200

    media queries first try

--- src/signin.php
+++ src/signin.php
@@ -4,6 +4,7 @@ include '../config/header.php';
 
 if ($_SESSION["state"] == 0)
 {
+    echo '<div id="phone">';
 	echo "<form class='form-signin' action='signin.php?signin' method='post' style='width: 40%'>";
 	echo "<h2 class='form-signin-heading'>Please sign in</h2>";
 	echo '<label for="inputEmail" class="sr-only">Email address</label>';
@@ -11,8 +12,8 @@ if ($_SESSION["state"] == 0)
     echo '<label for="inputPassword" class="sr-only">Password</label>';
     echo '<input type="password" style="margin: 2px" id="inputPassword" name="password" class="form-control" placeholder="Password" required>';
     echo '<button class="btn btn-lg btn-primary btn-block" style="margin: 2px" type="submit">Sign in</button>';
-	
 	echo "</form>";
+    echo '</div>';
 }
 else if (isset($_GET["signout"]))
 {
