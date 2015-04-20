62c3aaabffea5e2ce4333a884fe049a0d3577981
LucaNerlich <luca.nerlich@googlemail.com>
2015-04-20 20:02:46 +0200

    added margin to sign in form

--- src/signin.php
+++ src/signin.php
@@ -7,10 +7,10 @@ if ($_SESSION["state"] == 0)
 	echo "<form class='form-signin' action='signin.php?signin' method='post' style='width: 40%'>";
 	echo "<h2 class='form-signin-heading'>Please sign in</h2>";
 	echo '<label for="inputEmail" class="sr-only">Email address</label>';
-    echo '<input type="email" name="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>';
+    echo '<input type="email" style="margin: 2px" name="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>';
     echo '<label for="inputPassword" class="sr-only">Password</label>';
-    echo '<input type="password" id="inputPassword" name="password" class="form-control" placeholder="Password" required>';
-    echo '<button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>';
+    echo '<input type="password" style="margin: 2px" id="inputPassword" name="password" class="form-control" placeholder="Password" required>';
+    echo '<button class="btn btn-lg btn-primary btn-block" style="margin: 2px" type="submit">Sign in</button>';
 	
 	echo "</form>";
 }
