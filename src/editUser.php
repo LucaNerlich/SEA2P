adcb7f7f7df05b30df3ea574cc9958119f55bd85
Luca Nerlich <luca.nerlich@googlemail.com>
2015-05-15 18:37:24 +0200

    added user dropdown

--- src/editUser.php
+++ src/editUser.php
@@ -2,7 +2,19 @@
 include '../config/config.php';
 include '../config/header.php';
 
-echo "<h1>Gefahrene Kilometer eintragen</h1>";
+$kunden = query("SELECT * FROM client");
+
+echo "<h1>Welchen Kunden moechtest du bearbeiten?</h1>";
+
+echo "<br>";
+
+echo "<select name='client'>";
+
+foreach ($kunden as $kunde) {
+    echo "<option value='" . $kunde["client_id"] . "'>" . $kunde["name"] . ", " . $kunde["surname"] . " - " .  $kunde["client_id"]  . " </option>";
+}
+
+echo "</select>";
 
 include '../config/footer.php';
 ?>
