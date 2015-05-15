2f95cd8287f6dfa48f1bb4425f0a4b90930c684e
Luca Nerlich <luca.nerlich@googlemail.com>
2015-05-15 19:26:38 +0200

    edit user

--- src/editUser.php
+++ src/editUser.php
@@ -2,19 +2,35 @@
 include '../config/config.php';
 include '../config/header.php';
 
-$kunden = query("SELECT * FROM client");
-
 echo "<h1>Welchen Kunden moechtest du bearbeiten?</h1>";
-
+echo "<form action='editUser.php' method='post'>";
+$kunden = query("SELECT * FROM client");
 echo "<br>";
-
 echo "<select name='client'>";
 
 foreach ($kunden as $kunde) {
-    echo "<option value='" . $kunde["client_id"] . "'>" . $kunde["name"] . ", " . $kunde["surname"] . " - " .  $kunde["client_id"]  . " </option>";
+    echo "<option value='" . $kunde["client_id"] . "'>" . $kunde["name"] . ", " . $kunde["surname"] . " - " . $kunde["client_id"] . " </option>";
 }
 
 echo "</select>";
+
+echo "<input class='submit' type='submit' value='Kunden auswaehlen!'>";
+echo "</form>";
+
+if (isset($_POST["client"])){
+    $bike = query("SELECT * FROM bike WHERE client_id = " . $_kunde["client_id"]);
+    echo "<table>";
+    echo "<tr>";
+    echo "<td>Kunde: </td>";
+    echo "<td>" . $kunde["surname"] . "</td>";
+    echo "</tr>";
+    echo "<tr>";
+    echo "<td>Bike: </td>";
+    echo "<td>" . $bike["name"] . "</td>";
+    echo "</tr>";
+    echo "</table>";
+
+}
 
 include '../config/footer.php';
 ?>
