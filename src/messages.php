a9cd4d8d4689407d290b7ba6fc6a07d09cb66d2b
Fabian <Fabian@Laptop>
2015-06-01 09:25:45 +0200

    2

--- src/messages.php
+++ src/messages.php
@@ -16,7 +16,7 @@ if ($_SESSION["state"] == 1)
 		$topic = query("SELECT * FROM topics WHERE topic_id = $message_id");
 		if (sizeof($topic) > 0)
 		{
-			$messags = query("SELECT * FROM message WHERE topic_id = " . $topic["topic_id"]);
+			$messags = query("SELECT * FROM message WHERE topic_id = " . $message_id);
 		}
 		else
 		{
@@ -44,14 +44,17 @@ if ($_SESSION["state"] == 1)
 		echo "<h1>Deine Nachrichten</h1></div>";
 		$topics = query("SELECT * FROM topics WHERE client_id = " . $_SESSION["user"]["client_id"]);
 		echo "<div class='panel panel-default'>";
+		echo "<table class='table table-striped'>";
+		echo "<tr><th>Thema</th><th>Datum</th><th>Typ</th></tr>";
 		
 		foreach ($topics as $topic)
 		{
-			echo "<div class='panel-heading'><h2>" . $topic["created_on"] . ", Typ: " . ($topic["type"]==1?"Schadenmeldung":"Kontaktaufnahme") . "</h2></div>";
-			echo "<div class='panel-body'><a href='messages.php?topic_id=" . $topic["topic_id"] . "'>" . $topic["subject"] . "</a></div>";
+			//echo "<div class='panel-heading'><h2>" . $topic["created_on"] . ", Typ: " . ($topic["type"]==1?"Schadenmeldung":"Kontaktaufnahme") . "</h2></div>";
+			//echo "<div class='panel-body'><a href='messages.php?topic_id=" . $topic["topic_id"] . "'>" . $topic["subject"] . "</a></div>";
+			echo "<tr><td><a href='messages.php?topic_id=" . $topic["topic_id"] . "'>" . $topic["subject"] . "</a></td><td>" . $topic["created_on"] . "</td><td>" . ($topic["type"]==1?"Schadenmeldung":"Kontaktaufnahme") . "</td></tr>";
 		
 		}
-		
+		echo "</table>";
 		echo "</div>";
 	}
 
